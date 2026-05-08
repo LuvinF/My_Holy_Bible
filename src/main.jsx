6fbbeff4958a43f614047811a8d294603c1533a7
LuvinF <luvinfernandes26@gmail.com>
2026-05-08 17:17:28 +0530

    Fixed the Analytics to the app

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
-    <Analytics>
     <App />
-    </Analytics>
+    <Analytics/>
   </React.StrictMode>,
 )
