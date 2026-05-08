ddb10231a3f9fb34a7b4bf746a6d543b4714528b
LuvinF <luvinfernandes26@gmail.com>
2026-05-08 17:26:24 +0530

    Added speed Insights to the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <App />
     <Analytics/>
+    <SpeedInsights/>
   </React.StrictMode>,
 )
