74c26845369afac7ea5ed4557d9a57304cd4675a
LuvinF <luvinfernandes26@gmail.com>
2026-05-08 17:22:31 +0530

    Fixed the main from next to react

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
