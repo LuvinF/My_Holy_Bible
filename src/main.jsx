2134f50b6ba84b7ea1fbc88d2f0aaf2e331a118f
LuvinF <luvinfernandes26@gmail.com>
2026-05-08 16:37:27 +0530

    Added Analytics to the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Analytics } from "@vercel/analytics/next"
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
+    <Analytics>
     <App />
+    </Analytics>
   </React.StrictMode>,
 )
